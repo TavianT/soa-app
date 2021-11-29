dd4596a2c0f96aabb16bddea1235e7801584b3f1
Tavian <taviantaylor00@gmail.com>
2021-11-29 23:57:14 +0000

    Added stub function

--- main/src/main/java/com/example/soa/api/User.java
+++ main/src/main/java/com/example/soa/api/User.java
@@ -1,5 +1,19 @@
 package com.example.soa.api;
 
-public class User {
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
+
+@Path("user")
+public class User {
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void createUser() {
+		
+	}
 }
