969bbc8aeadd8fd693df3c06953032bf892020d7
Tavian <taviantaylor00@gmail.com>
2021-12-18 01:34:37 +0000

    Request validation

--- soa/src/main/java/org/tavian/scc/soa/resources/ProposalResource.java
+++ soa/src/main/java/org/tavian/scc/soa/resources/ProposalResource.java
@@ -1,5 +1,10 @@
 package org.tavian.scc.soa.resources;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.tavian.scc.soa.models.Proposal;
 
 import jakarta.ws.rs.Consumes;
@@ -14,6 +19,28 @@ public class ProposalResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void createProposal(Proposal proposal) {
-		
+		//error checks
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+		try {
+			Date tripDate = sdf.parse(proposal.getTripProposalDate());
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.DATE, 14);
+			Date dateFourteenDaysFromCurrent = calendar.getTime();
+			if (tripDate.after(dateFourteenDaysFromCurrent)) {
+				throw new WebApplicationException(Response.Status.BAD_REQUEST);
+			}
+			if (Integer.parseInt(proposal.getLocation().getLatitude()) < -90 && Integer.parseInt(proposal.getLocation().getLatitude()) > 90) {
+				throw new WebApplicationException(Response.Status.BAD_REQUEST);
+			}
+			if (Integer.parseInt(proposal.getLocation().getLongitude()) < -180 && Integer.parseInt(proposal.getLocation().getLongitude()) > 180) {
+				throw new WebApplicationException(Response.Status.BAD_REQUEST);
+			}
+			
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+		}
 	}
 }
